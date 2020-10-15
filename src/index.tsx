c99aff7dc29913eaefe3cf31c567f1e64326267b
Ken Duigraha Putra <kenduigraha@gmail.com>
2020-10-15 22:01:46 +0700

    Add library for testing & setup redux devtools extension.

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,24 @@
 // eslint-disable-next-line
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createStore, applyMiddleware, Store } from 'redux';
+import { Provider } from 'react-redux';
+import thunk from 'redux-thunk';
+import { composeWithDevTools } from 'redux-devtools-extension';
 import './index.css';
 import App from './App';
+import reducer from './store/reducer';
 import * as serviceWorker from './serviceWorker';
+
+const store: Store<ArticleState, ArticleAction> & {
+  dispatch: DispatchType;
+} = createStore(reducer, composeWithDevTools(applyMiddleware(thunk)));
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root'),
 );
