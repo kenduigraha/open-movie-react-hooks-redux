a1371416ed77faaf6108e175bf03b5b01b0528a7
Ken Duigraha Putra <kenduigraha@gmail.com>
2020-10-15 13:42:26 +0700

    Install Ant Design.

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,19 @@
-import React from 'react';
+import React, { useRef } from 'react';
+import { Button, Alert } from 'antd';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const btnElement = useRef(null);
+  
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
+        <div>
+          <Alert message="Success Text" type="success" />
+          <Button ref={btnElement} type="primary" >Button Ant Design</Button>
+        </div>
       </header>
     </div>
   );
